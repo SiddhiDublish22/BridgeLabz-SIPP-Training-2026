abab7911191313fe2422573acae48b92f48b4d2c
SiddhiDublish22 <siddhidublish.22@gmail.com>
2026-06-08 11:07:55 +0530

    Variables Types

---
+++ core-java-practice/gcr-codebase/programming-elements/VariableTypes.java
@@ -0,0 +1,34 @@
+public class VariableTypes{
+
+  static int x; //no usage
+  int y; //1 usage
+
+  static void m1(){
+    System.out.println("m1");
+    System.out.println(x);
+    VariableTypes obj = new VariableTypes();
+    System.out.println(obj.y);
+  }
+
+  void m2(){
+    int y = 40;
+    System.out.println("m2");
+    System.out.println(x);
+    System.out.println(y);
+    System.out.println(this.y); //current method for object specify
+  }
+
+  public static void main(String[] args){
+    int x = 10;
+    int e = 45;
+    System.out.println(x);
+    System.out.println(VariableTypes.x);
+    x=20;
+    System.out.println(x);
+    VariableTypes.x = 30;
+    VariableTypes obj = new VariableTypes();
+    System.out.println(obj.y);
+    m1();
+    obj.m2();
+  }
+}
