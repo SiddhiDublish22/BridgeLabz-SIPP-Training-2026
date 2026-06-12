5487657daa832bf81709e6b6683963b3d1b771e2
SiddhiDublish22 <siddhidublish.22@gmail.com>
2026-06-12 19:57:34 +0530

    [Siddhi] Update basic.java

--- core-java-practice/gcr-codebase/programming-elements/String/basic.java
+++ core-java-practice/gcr-codebase/programming-elements/String/basic.java
@@ -2,50 +2,50 @@ import java.util.*;
 
 public class basic {
   public static void main(String[] args){
-    // String s = "Siddhi";
-    // System.out.println(s);
-    // System.out.println(s.length());
-    // System.out.println(s.charAt(0));
-    // System.out.println(s.charAt(1));
+    String s = "Siddhi";
+    System.out.println(s);
+    System.out.println(s.length());
+    System.out.println(s.charAt(0));
+    System.out.println(s.charAt(1));
 
     // //string comparison
-    // String s1 = "hello";
-    // String s2 = "hello";
-    // String s3 = new String("hello");
-    // System.out.println(s1.equals(s2)); // true
-    // System.out.println(s1 == s2); //avoid this
-    // System.out.println(s1 == s3);
+    String s1 = "hello";
+    String s2 = "hello";
+    String s3 = new String("hello");
+    System.out.println(s1.equals(s2)); // true
+    System.out.println(s1 == s2); //avoid this
+    System.out.println(s1 == s3);
 
     //string immutability
-    // String s = "hello";
-    // s.toUpperCase();
-    // System.out.println(s); //still "hello"
+    String s = "hello";
+    s.toUpperCase();
+    System.out.println(s); //still "hello"
 
-    // s=s.toUpperCase();
-    // System.out.println(s); //"HELLO"
+    s=s.toUpperCase();
+    System.out.println(s); //"HELLO"
 
     //reverse string
-    // String s = "hello";
-    // String result ="";
+    String s = "hello";
+    String result ="";
 
-    // for(int i=s.length()-1; i>=0; i--){
-    //   result = result + s.charAt(i);
-    // }
-    // System.out.println(result);
+    for(int i=s.length()-1; i>=0; i--){
+      result = result + s.charAt(i);
+    }
+    System.out.println(result);
 
     //palindrom check
-    // String s = "madam";
-    // String reversed = "";
+    String s = "madam";
+    String reversed = "";
 
-    // for(int i=s.length()-1; i>=0; i--){
-    //   reversed = reversed + s.charAt(i);
-    // }
-    // if(s.equals(reversed)){
-    //   System.out.println("Palindrom");
-    // }
-    // else{
-    //   System.out.println("Not palindrom");
-    // }
+    for(int i=s.length()-1; i>=0; i--){
+      reversed = reversed + s.charAt(i);
+    }
+    if(s.equals(reversed)){
+      System.out.println("Palindrom");
+    }
+    else{
+      System.out.println("Not palindrom");
+    }
 
     //string to char array
     String s = "hello";
